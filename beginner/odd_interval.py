488c1973e2b7a12f00803d95d3ed9cb0c8617438
ParameshwariM <36121090+ParameshwariM@users.noreply.github.com>
2018-03-01 14:24:45 +0530

    Create odd_interval.py

---
+++ beginner/odd_interval.py
@@ -0,0 +1,14 @@
+i=int(input(""))
+n=int(input(""))
+if (i and n<=100000):
+        for a in range(i,n):  
+                if((a%2)!=0):
+                        if(a>i):
+                                print(a)
+                         
+                 
+            
+                 
+else:
+    print("No")
+    
